2ae91a6ba9421412e35b19ca9b798ef19babb4ce
carlos-ventura <carlospinho_pt@hotmail.com>
2022-02-01 15:02:24 +0100

    Prepare data with yfinance and cryptocmd

--- thesis.py
+++ thesis.py
@@ -2,8 +2,28 @@
 
 # imports
 import pandas as pd
-import yfinance as yf
 import numpy as np
 import datetime
+import yfinance as yf
 
+from cryptocmd import CmcScraper
 import constants as c
+
+all_data_tickers = {}
+
+crypto_tickers = c.CRYPTO_BASKET
+crypto_rates = c.APPLIED_CRYPTO_RATES
+
+for ticker in crypto_tickers:
+    ticker_scraper = CmcScraper(ticker)
+    ticker_df = ticker_scraper.get_dataframe()
+    ticker_df = ticker_df[ticker_df['Close'].notna()]
+    ticker_rates = crypto_rates[ticker]
+    data = {'returns': ticker_df, 'annual_passive_rates': ticker_rates}
+    all_data_tickers.setdefault(ticker, data)
+
+print(all_data_tickers)
+
+# stock = yf.download('BTC-USD', period="max")
+# stock = stock[stock['Adj Close'].notna()]
+# print(stock)
