21fef3fedc8ff83c36f4ab0bbacf02c5eb2648bb
carlos-ventura <carlospinho_pt@hotmail.com>
2022-02-01 15:56:02 +0100

    Add apr and apy converter functions

--- utils.py
+++ utils.py
@@ -2,13 +2,31 @@
 File with multiple helper functions
 """
 
-
-def apy_to_apr(rate):
-    pass
+PERIODS = 12  # Number of months per year (Assumption nr 1)
 
 
-def apr_to_apy(rate):
-    pass
+def apy_to_apr(rate, percentage=False):
+    """
+    Params: annual percentage yield (APY)
+    Function: Convert APY to APR
+    """
+    rate = float(rate)
+    if percentage:
+        rate /= 100
+    periodic_rate = (1 + rate)**(1/PERIODS) - 1
+    return (periodic_rate * PERIODS) * 100
+
+
+def apr_to_apy(rate, percentage=False):
+    """
+    Params: annual percentage rate (APR)
+    Function: Convert APR to APY
+    """
+    rate = float(rate)
+    if percentage:
+        rate /= 100
+    periodic_rate = float(rate) / PERIODS
+    return ((1 + periodic_rate)**PERIODS - 1) * 100
 
 
 def daily_to_annualy(rate, percentage=False):
