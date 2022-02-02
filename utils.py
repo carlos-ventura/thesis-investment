b3e0ebbc47e3367ea9d00568194ae896061624b0
carlos-ventura <carlospinho_pt@hotmail.com>
2022-02-02 14:23:58 +0100

    Add annual to daily rate function

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ File with multiple helper functions
 """
 
 PERIODS = 12  # Number of months per year (Assumption nr 1)
+DAYS = 365  # Number of days per year (Cryptocurrency)
 
 
 def apy_to_apr(rate, percentage=False):
@@ -13,7 +14,7 @@ def apy_to_apr(rate, percentage=False):
     rate = float(rate)
     if percentage:
         rate /= 100
-    periodic_rate = (1 + rate)**(1/PERIODS) - 1
+    periodic_rate = (1 + rate) ** (1 / PERIODS) - 1
     return periodic_rate * PERIODS
 
 
@@ -26,16 +27,28 @@ def apr_to_apy(rate, percentage=False):
     if percentage:
         rate /= 100
     periodic_rate = float(rate) / PERIODS
-    return (1 + periodic_rate)**PERIODS - 1
+    return (1 + periodic_rate) ** PERIODS - 1
 
 
 def daily_to_annualy(rate, percentage=False):
     """
     Params: daily rate
-    Function: convert daily rate to annual rate (365 days in 1 year - crypto)
-    Return: annualized  rate
+    Function: convert daily rate to annual rate
+    Return: annualized rate
     """
     rate = float(rate)
     if percentage:
         rate /= 100
-    return (1 + rate) ** 365 - 1
+    return (1 + rate) ** DAYS - 1
+
+
+def annualy_to_daily(rate, percentage=False):
+    """
+    Params: annual rate
+    Function: convert annual rate to daily rate
+    Return: daily rate
+    """
+    rate = float(rate)
+    if percentage:
+        rate /= 100
+    return (1 + rate) ** (1 / DAYS) - 1
