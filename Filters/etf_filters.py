a9dd6dd54b5e3bba75a0259054395796915f5a3c
carlos-ventura <carlospinho_pt@hotmail.com>
2022-04-26 18:36:58 +0200

    Add new expense ratio filter

--- Filters/etf_filters.py
+++ Filters/etf_filters.py
@@ -1,13 +1,18 @@
 import time
-from utils_filters import volume_filter
+from utils_filters import volume_filter, expense_ratio_filter_yf
 
 ETF_FILENAME = '../data/etf_tickers.txt'
+ETF_VOLUME_FILTERED_FILENAME = '../data/etf_tickers_volume_filtered.txt'
+ETF_ER_FILTERED_TICKERS = '../data/etf_tickers_er_filtered.txt'
 MINIMUM_VOLUME_2021 = 50000
+MAXIMUM_EXPENSE_RATIO = 0.005
 VOLUME_START_DATE = '2021-01-01'
 VOLUME_END_DATE = '2022-01-01'
 
 if __name__ == '__main__':
     start_time = time.time()
-    volume_filtered_etf_tickers = volume_filter(ETF_FILENAME, VOLUME_START_DATE, VOLUME_END_DATE, MINIMUM_VOLUME_2021, 'etf')
-    print(len(volume_filtered_etf_tickers))
-    print(f"Filter took {time.time() - start_time}s to run")
+    # volume_filtered_etf_tickers = volume_filter(ETF_FILENAME, VOLUME_START_DATE, VOLUME_END_DATE, MINIMUM_VOLUME_2021, 'etf')
+    # print(len(volume_filtered_etf_tickers))
+
+    er_filtered_etf_tickers = expense_ratio_filter_yf(ETF_VOLUME_FILTERED_FILENAME, MAXIMUM_EXPENSE_RATIO, True, True)
+    print(f"Filters took {time.time() - start_time}s to run")
--- Filters/utils_filters.py
+++ Filters/utils_filters.py
@@ -1,6 +1,10 @@
+from itertools import repeat
 import json
 import yfinance as yf
 import numpy as np
+import concurrent.futures
+import time
+import requests_cache
 
 def date_filter(filename:str, start_date:str, ticker_type:str):
     tickers = []
@@ -51,3 +55,53 @@ def rates_filter(filename:str):
         txt_rates_filtered.write("\n".join(map(str, new_tickers)))
 
     return new_tickers
+
+def expense_ratio_filter_yf(filename:str, maximum:float, check_filtered=False, check_none=False):
+    session = requests_cache.CachedSession('dns.cache')
+    session.headers['User-agent'] = 'Opera/9.60 (Windows NT 6.0; U; en) Presto/2.1.1'
+    tickers = []
+    new_tickers = []
+    none_tickers = []
+    with open(filename, "r", encoding="UTF-8") as ticker_file:
+        tickers = ticker_file.read().split('\n')
+    if check_filtered:
+        with open('../data/etf_tickers_er_filtered.txt', "r", encoding="UTF-8") as ticker_file:
+            new_tickers = ticker_file.read().split('\n')
+            if not check_none:
+                tickers = list(set(tickers) - set(new_tickers))
+    if check_none:
+        with open('../data/etf_tickers_er_none.txt', "r", encoding="UTF-8") as ticker_file:
+            tickers = ticker_file.read().split('\n')
+
+    tickers_to_analyse = tickers
+    chunk_size = 5
+    chunks = [tickers_to_analyse[i:i+chunk_size] for i in range(0,len(tickers_to_analyse),chunk_size)]
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = executor.map(er_helper, chunks, repeat(maximum), repeat(session))
+        for result in results:
+            new_tickers.extend(result['new_tickers'])
+            none_tickers.extend(result['none_tickers'])
+
+    with open('../data/etf_tickers_er_filtered.txt', 'w', encoding='UTF-8') as txt_er_filtered:
+        txt_er_filtered.write("\n".join(map(str, new_tickers)))
+
+    with open('../data/etf_tickers_er_none.txt', 'w', encoding='UTF-8') as txt_er_filtered:
+        txt_er_filtered.write("\n".join(map(str, none_tickers)))
+
+    return new_tickers
+
+def er_helper(chunk:list, maximum:float, session):
+    new_tickers = []
+    none_tickers = []
+    tickers_data = [yf.Ticker(t, session=session) for t in chunk]
+    for ticker in tickers_data:
+        time.sleep(1)
+        expense_ratio = ticker.info['annualReportExpenseRatio']
+        print(f"{ticker.ticker} ::: {expense_ratio}")
+        if expense_ratio is None:
+            none_tickers.append(ticker.ticker)
+            continue
+        if expense_ratio <= maximum:
+            new_tickers.append(ticker.ticker)
+            continue
+    return {'new_tickers': new_tickers, 'none_tickers': none_tickers}
