438325967ffd5ef59db232a526553eeb8ee4d5fb
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-01 19:03:19 +0200

    Filter ETF by 2021 date

--- Filters/etf_filters.py
+++ Filters/etf_filters.py
@@ -1,5 +1,5 @@
 import time
-from utils_filters import volume_filter, expense_ratio_filter_yf
+from utils_filters import volume_filter, expense_ratio_filter_yf, date_filter
 
 ETF_FILENAME = '../data/etf_tickers.txt'
 ETF_VOLUME_FILTERED_FILENAME = '../data/etf_tickers_volume_filtered.txt'
@@ -14,5 +14,7 @@ if __name__ == '__main__':
     # volume_filtered_etf_tickers = volume_filter(ETF_FILENAME, VOLUME_START_DATE, VOLUME_END_DATE, MINIMUM_VOLUME_2021, 'etf')
     # print(len(volume_filtered_etf_tickers))
 
-    er_filtered_etf_tickers = expense_ratio_filter_yf(ETF_VOLUME_FILTERED_FILENAME, MAXIMUM_EXPENSE_RATIO, True, True)
-    print(f"Filters took {time.time() - start_time}s to run")
+    # er_filtered_etf_tickers = expense_ratio_filter_yf(ETF_VOLUME_FILTERED_FILENAME, MAXIMUM_EXPENSE_RATIO, True, True)
+    # print(f"Filters took {time.time() - start_time}s to run")
+
+    date_filtered_etf_tickers = date_filter(ETF_ER_FILTERED_TICKERS, VOLUME_START_DATE, 'etf')
