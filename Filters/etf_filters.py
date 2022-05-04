2ec6e2dc9279a324e2db64eb69a8fedb170b5902
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-04 14:52:29 +0200

    wip

--- Filters/etf_filters.py
+++ Filters/etf_filters.py
@@ -1,13 +1,15 @@
 import time
 from utils_filters import volume_filter, expense_ratio_filter_yf, date_filter
 
-ETF_FILENAME = '../data/etf_tickers.txt'
-ETF_VOLUME_FILTERED_FILENAME = '../data/etf_tickers_volume_filtered.txt'
-ETF_ER_FILTERED_TICKERS = '../data/etf_tickers_er_filtered.txt'
+ETF_FILENAME = '../data/etf.txt'
+ETF_VOLUME_FILTERED_FILENAME = '../data/etf-volume-f.txt'
+ETF_ER_FILTERED_TICKERS = '../data/etf-er-f.txt'
 MINIMUM_VOLUME_2021 = 50000
 MAXIMUM_EXPENSE_RATIO = 0.005
 VOLUME_START_DATE = '2021-01-01'
 VOLUME_END_DATE = '2022-01-01'
+
+DATE_ARRAY = ['2020-01-01', '2019-01-01', '2018-01-01', '2017-01-01', '2016-01-01']
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -15,6 +17,9 @@ if __name__ == '__main__':
     # print(len(volume_filtered_etf_tickers))
 
     # er_filtered_etf_tickers = expense_ratio_filter_yf(ETF_VOLUME_FILTERED_FILENAME, MAXIMUM_EXPENSE_RATIO, True, True)
-    # print(f"Filters took {time.time() - start_time}s to run")
 
-    date_filtered_etf_tickers = date_filter(ETF_ER_FILTERED_TICKERS, VOLUME_START_DATE, 'etf')
+    for date in DATE_ARRAY:
+        date_filter(ETF_ER_FILTERED_TICKERS, start_date=date, ticker_type='etf', target_name=date.split('-', maxsplit=1)[0])
+    # date_filter(ETF_ER_FILTERED_TICKERS, VOLUME_START_DATE, 'etf')
+
+    print(f"Filters took {time.time() - start_time}s to run")
--- test.py
+++ test.py
@@ -1,36 +1,51 @@
-from ast import AsyncFor
 import yfinance as yf
 import pandas as pd
 from scipy.stats.mstats import gmean
 
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
+
 from utils import convert_to_annual
 
-ticker = yf.download('SPY', interval='1d')['Adj Close']
+# ticker = yf.download('SPY', interval='1d')['Adj Close']
 
 
-prices = ticker[pd.notna(ticker)]
-returns = prices.pct_change()
-daily_geomean_return = gmean(1 + returns.to_numpy()[1:]) - 1
-annual_geomean_return = convert_to_annual(daily_geomean_return, 'd')
+# prices = ticker[pd.notna(ticker)]
+# returns = prices.pct_change()
+# daily_geomean_return = gmean(1 + returns.to_numpy()[1:]) - 1
+# annual_geomean_return = convert_to_annual(daily_geomean_return, 'd')
 
-print(annual_geomean_return)
+# print(annual_geomean_return)
 
 ticker = yf.download('VWCE.DE', interval='1wk')['Adj Close']
 
 
-prices = ticker[pd.notna(ticker)]
+prices = ticker[pd.notna(ticker)].to_frame()
 returns = prices.pct_change()
 daily_geomean_return = gmean(1 + returns.to_numpy()[1:]) - 1
+std = returns.std()
+annual_Std = convert_to_annual(std, 'w', std=True)
 annual_geomean_return = convert_to_annual(daily_geomean_return, 'w')
 
 print(annual_geomean_return)
+print(annual_Std)
 
-ticker = yf.download('VWCE.DE', interval='1mo')['Adj Close']
+# Calculate expected returns and sample covariance
+mu = expected_returns.mean_historical_return(prices, compounding=True, frequency=52)
+S = risk_models.sample_cov(prices, frequency=52)
+
+# Optimize for maximal Sharpe ratio
+ef = EfficientFrontier(mu, S)
+weights = ef.max_sharpe()
+ef.portfolio_performance(verbose=True)
+
+# ticker = yf.download('VWCE.DE', interval='1mo')['Adj Close']
 
 
-prices = ticker[pd.notna(ticker)]
-returns = prices.pct_change()
-daily_geomean_return = gmean(1 + returns.to_numpy()[1:]) - 1
-annual_geomean_return = convert_to_annual(daily_geomean_return, 'm')
+# prices = ticker[pd.notna(ticker)]
+# returns = prices.pct_change()
+# daily_geomean_return = gmean(1 + returns.to_numpy()[1:]) - 1
+# annual_geomean_return = convert_to_annual(daily_geomean_return, 'm')
 
-print(annual_geomean_return)
+# print(annual_geomean_return)
