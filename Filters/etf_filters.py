8e07b9ed4fc980dd37a01f7f9b1a2350907b00ac
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-11 01:27:59 +0200

    Etf filtering

--- Filters/etf_filters.py
+++ Filters/etf_filters.py
@@ -1,25 +1,32 @@
 import time
 from utils_filters import volume_filter, expense_ratio_filter_yf, date_filter
 
-ETF_FILENAME = '../data/etf.txt'
+ETF_FILENAME = '../data/etf-tickers.txt'
+ETF_PREP_VOL = '../data/etf-date-vol-prep-f.txt'
 ETF_VOLUME_FILTERED_FILENAME = '../data/etf-volume-f.txt'
 ETF_ER_FILTERED_TICKERS = '../data/etf-er-f.txt'
 MINIMUM_VOLUME_2021 = 50000
 MAXIMUM_EXPENSE_RATIO = 0.005
-VOLUME_START_DATE = '2021-01-01'
-VOLUME_END_DATE = '2022-01-01'
+VOLUME_START_DATE = '2021-05-01'
+VOLUME_END_DATE = '2022-05-01'
 
-DATE_ARRAY = ['2020-01-01', '2019-01-01', '2018-01-01', '2017-01-01', '2016-01-01']
+DATE_ARRAY = ['2020-05-01', '2019-05-01', '2018-05-01', '2017-05-01']
 
 if __name__ == '__main__':
     start_time = time.time()
-    # volume_filtered_etf_tickers = volume_filter(ETF_FILENAME, VOLUME_START_DATE, VOLUME_END_DATE, MINIMUM_VOLUME_2021, 'etf')
-    # print(len(volume_filtered_etf_tickers))
 
-    # er_filtered_etf_tickers = expense_ratio_filter_yf(ETF_VOLUME_FILTERED_FILENAME, MAXIMUM_EXPENSE_RATIO, True, True)
+    # date_filter(ETF_FILENAME, VOLUME_START_DATE, 'etf', 'date-vol-prep')
+
+    # while True:
+    # expense_ratio_filter_yf(ETF_PREP_VOL, MAXIMUM_EXPENSE_RATIO)
+
+    # volume_filter(ETF_ER_FILTERED_TICKERS, VOLUME_START_DATE, VOLUME_END_DATE, MINIMUM_VOLUME_2021, 'etf')
+
 
     for date in DATE_ARRAY:
-        date_filter(ETF_ER_FILTERED_TICKERS, start_date=date, ticker_type='etf', target_name=date.split('-', maxsplit=1)[0])
+        date_filter(ETF_VOLUME_FILTERED_FILENAME, start_date=date, ticker_type='etf', target_name=date.split('-', maxsplit=1)[0])
+        print(f"Finished date filter for {date}")
+        time.sleep(60)
     # date_filter(ETF_ER_FILTERED_TICKERS, VOLUME_START_DATE, 'etf')
 
     print(f"Filters took {time.time() - start_time}s to run")
--- Filters/utils_filters.py
+++ Filters/utils_filters.py
@@ -2,6 +2,7 @@ import concurrent.futures
 import json
 import time
 from itertools import repeat
+import os
 
 import numpy as np
 import requests_cache
@@ -29,7 +30,8 @@ def volume_filter(filename:str, start_date:str, end_date:str, minimum:int, ticke
     with open(filename, "r", encoding="UTF-8") as ticker_file:
         tickers = ticker_file.read().split('\n')
 
-    tickers_data = yf.download(tickers, start=start_date, end=end_date).sort_index()
+    tickers_data = yf.download(tickers, start=start_date, end=end_date, interval='1wk').sort_index()
+    tickers_data.dropna(how='all', inplace=True)
     print(tickers_data)
     if ticker_type == 'etf':
         new_tickers = [ticker for ticker in tickers if tickers_data['Volume'][ticker].mean() >= minimum]
@@ -54,31 +56,49 @@ def rates_filter(filename:str):
     with open('../data/crypto-rates-f.txt', 'w', encoding='UTF-8') as txt_rates_filtered:
         txt_rates_filtered.write("\n".join(map(str, new_tickers)))
 
-def expense_ratio_filter_yf(filename:str, maximum:float, check_filtered=False, check_none=False):
-    session = requests_cache.CachedSession('dns.cache')
-    session.headers['User-agent'] = 'Opera/9.60 (Windows NT 6.0; U; en) Presto/2.1.1'
-    tickers = []
+def expense_ratio_filter_yf(filename:str, maximum:float):
+    session = requests_cache.CachedSession('yahoo_finance.cache')
+    session.headers['User-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41'
     new_tickers = []
     none_tickers = []
-    with open(filename, "r", encoding="UTF-8") as ticker_file:
-        tickers = ticker_file.read().split('\n')
-    if check_filtered:
-        with open('../data/etf-er-f.txt', "r", encoding="UTF-8") as ticker_file:
-            new_tickers = ticker_file.read().split('\n')
-            if not check_none:
-                tickers = list(set(tickers) - set(new_tickers))
-    if check_none:
-        with open('../data/etf-er-none.txt', "r", encoding="UTF-8") as ticker_file:
-            tickers = ticker_file.read().split('\n')
+    above_tickers= []
 
-    tickers_to_analyse = tickers
+    with open(filename, "r", encoding="UTF-8") as ticker_file:
+        none_tickers = ticker_file.read().split('\n')
+
+    if not os.path.exists('../data/etf-er-none.txt'):
+        with open('../data/etf-er-none.txt', "w", encoding="UTF-8") as ticker_none_file:
+            ticker_none_file.write("\n".join(map(str, none_tickers)))
+    else:
+         with open('../data/etf-er-none.txt', 'r', encoding='UTF-8') as ticker_none_file:
+             none_tickers = ticker_none_file.read().split("\n")
+
+    if os.path.exists('../data/etf-er-above.txt'):
+        with open('../data/etf-er-above.txt', 'r', encoding='UTF-8') as ticker_above_file:
+            above_tickers = ticker_above_file.read().split("\n")
+
+    if os.path.exists('../data/etf-er-f.txt'):
+        with open('../data/etf-er-f.txt', 'r', encoding='UTF-8') as ticker_f_file:
+            new_tickers = ticker_f_file.read().split("\n")
+
+    none_tickers = none_tickers[25:]
+    tickers_to_analyse = none_tickers[:25]
+
     chunk_size = 5
     chunks = [tickers_to_analyse[i:i+chunk_size] for i in range(0,len(tickers_to_analyse),chunk_size)]
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = executor.map(er_helper, chunks, repeat(maximum), repeat(session))
+        temp_none = []
         for result in results:
+            print(f" new tickers : {len(result['new_tickers'])} \n none tickers : {len(result['none_tickers'])} \n above tickers : {len(result['above_tickers'])}")
             new_tickers.extend(result['new_tickers'])
-            none_tickers.extend(result['none_tickers'])
+            temp_none.extend(result['none_tickers'])
+            above_tickers.extend(result['above_tickers'])
+
+    none_tickers.extend(temp_none)
+
+    print("\nWritting to files...\n")
+    time.sleep(1)
 
     with open('../data/etf-er-f.txt', 'w', encoding='UTF-8') as txt_er_filtered:
         txt_er_filtered.write("\n".join(map(str, new_tickers)))
@@ -86,10 +106,14 @@ def expense_ratio_filter_yf(filename:str, maximum:float, check_filtered=False, c
     with open('../data/etf-er-none.txt', 'w', encoding='UTF-8') as txt_er_filtered:
         txt_er_filtered.write("\n".join(map(str, none_tickers)))
 
+    with open('../data/etf-er-above.txt', 'w', encoding='UTF-8') as txt_er_filtered:
+        txt_er_filtered.write("\n".join(map(str, above_tickers)))
+
 
 def er_helper(chunk:list, maximum:float, session):
     new_tickers = []
     none_tickers = []
+    above_tickers = []
     tickers_data = [yf.Ticker(t, session=session) for t in chunk]
     for ticker in tickers_data:
         time.sleep(1)
@@ -101,7 +125,8 @@ def er_helper(chunk:list, maximum:float, session):
         if expense_ratio <= maximum:
             new_tickers.append(ticker.ticker)
             continue
-    return {'new_tickers': new_tickers, 'none_tickers': none_tickers}
+        above_tickers.append(ticker.ticker)
+    return {'new_tickers': new_tickers, 'none_tickers': none_tickers, 'above_tickers': above_tickers}
 
 def mst_filter(filename:str):
     with open(filename, "r", encoding="UTF-8") as ticker_file:
@@ -110,13 +135,11 @@ def mst_filter(filename:str):
     tickers_data = yf.download(tickers[:100], start="2021-01-01", end="2022-01-01", interval="1wk")["Adj Close"]
     tickers_data.dropna(inplace=True)
     tickers_return = tickers_data.pct_change()[1:] # Remove first row of NaN values
-    print(tickers_return)
     new_tickers = []
 
     while tickers_return.shape[1] > 30:
-        new_tickers,tickers_return,_,_ = MinimumSpanningTree(tickers_return)
+        new_tickers,_,_,_ = MinimumSpanningTree(tickers_return)
 
-    print(tickers_return)
     print(new_tickers)
 
-mst_filter('../data/etf-date-f.txt')
+# mst_filter('../data/etf-date-f.txt')
