8bf90e662c0e892e34e234f3e52ebe624205ca95
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-11 02:04:45 +0200

    Crypto filtering

--- Filters/crypto_filters.py
+++ Filters/crypto_filters.py
@@ -1,20 +1,29 @@
 import time
-from utils_filters import rates_filter, volume_filter
+from utils_filters import rates_filter, volume_filter, date_filter
 
 SOURCE_CRYPTO = '../data/crypto_tickers.txt'
-CRYPTO_FILTERED_VOLUME = '../data/crypto_tickers_volume_filtered.txt'
-CRYPTO_FILTERED_RATES = '../data/crypto_tickers_rates_filtered.txt'
+CRYPTO_FILTERED_VOLUME = '../data/crypto-volume-f.txt'
+CRYPTO_FILTERED_RATES = '../data/crypto-rates-f.txt'
+CRYPTO_PREP_VOL = '../data/crypto-date-vol-prep-f.txt'
 
 MINIMUM_VOLUME_2021 = 50000
-VOLUME_START_DATE = '2021-01-01'
-VOLUME_END_DATE = '2022-01-01'
+VOLUME_START_DATE = '2021-05-01'
+VOLUME_END_DATE = '2022-05-01'
+
+DATE_ARRAY = ['2021-05-01', '2020-05-01', '2019-05-01', '2018-05-01', '2017-05-01']
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    rates_filtered_crypto_tickers = rates_filter(SOURCE_CRYPTO)
-    print(f'{len(rates_filtered_crypto_tickers)} crypto tickers after rates filter')
-    volume_filtered_crypto_tickers = volume_filter(CRYPTO_FILTERED_RATES, VOLUME_START_DATE, VOLUME_END_DATE, MINIMUM_VOLUME_2021, 'crypto')
-    print(f'{len(volume_filtered_crypto_tickers)} crypto tickers after volume filter')
+    # rates_filter(SOURCE_CRYPTO)
+
+    # date_filter(CRYPTO_FILTERED_RATES, VOLUME_START_DATE, 'crypto', 'date-vol-prep')
+    # volume_filter(CRYPTO_PREP_VOL, VOLUME_START_DATE, VOLUME_END_DATE, MINIMUM_VOLUME_2021, 'crypto')
+
+    for date in DATE_ARRAY:
+        time.sleep(60)
+        date_filter(CRYPTO_FILTERED_VOLUME, start_date=date, ticker_type='crypto', target_name=date.split('-', maxsplit=1)[0])
+        print(f"Finished date filter for {date}")
+
 
     print(f"Filter took {time.time() - start_time}s to run")
