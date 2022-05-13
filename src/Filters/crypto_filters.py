94eff09f17637f1784a96b773cf7fd2fe2f7976a
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-13 17:32:35 +0200

    Update crypto and etf filters main

--- src/Filters/crypto_filters.py
+++ src/Filters/crypto_filters.py
@@ -1,27 +1,23 @@
 import time
 from src.Filters.utils_filters import volume_filter, expense_ratio_filter_yf, date_filter,mst_filter
 
+import src.constants as c
+
 SOURCE_CRYPTO = '../data/crypto_tickers.txt'
 CRYPTO_FILTERED_VOLUME = '../data/crypto-volume-f.txt'
 CRYPTO_FILTERED_RATES = '../data/crypto-rates-f.txt'
 CRYPTO_PREP_VOL = '../data/crypto-date-vol-prep-f.txt'
 
-MINIMUM_DAILY_VOLUME = 50000
-VOLUME_START_DATE = '2021-05-01'
-VOLUME_END_DATE = '2022-05-01'
-
-DATE_ARRAY = ['2017-11-06', '2018-05-01', '2019-05-01', '2020-05-01', '2021-05-01']
-
 if __name__ == '__main__':
     start_time = time.time()
     # rates_filter(SOURCE_CRYPTO)
-    for date in DATE_ARRAY:
+    for date in c.START_DATES:
         target_name=date.split('-', maxsplit=1)[0]
-        filename = f'../data/crypto-{target_name}-f.txt'
+        filename = [f'../data/crypto-{target_name}-f.txt']
         date_filter(CRYPTO_FILTERED_RATES, start_date=date, ticker_type='crypto', target_name=target_name)
-        volume_filter(filename, date, VOLUME_END_DATE, MINIMUM_DAILY_VOLUME, 'crypto')
-        mst_filter([filename], start_date=date, end_date=VOLUME_END_DATE, target_name=target_name, ticker_type='crypto', min_sr=False)
-        mst_filter([filename], start_date=date, end_date=VOLUME_END_DATE, target_name=target_name, ticker_type='crypto', min_sr=True, sr_value=0)
-        mst_filter([filename], start_date=date, end_date=VOLUME_END_DATE, target_name=target_name, ticker_type='crypto', min_sr=True, sr_value=1)
+        volume_filter(filename[0], date, c.END_DATE, c.MINIMUM_DAILY_VOLUME, 'crypto')
+        mst_filter(filename, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='crypto', min_sr=False)
+        mst_filter(filename, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='crypto', min_sr=True, sr_value=0)
+        mst_filter(filename, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='crypto', min_sr=True, sr_value=1)
 
     print(f"Filter took {time.time() - start_time}s to run")
--- src/Filters/etf_filters.py
+++ src/Filters/etf_filters.py
@@ -1,37 +1,28 @@
 import time
 from src.Filters.utils_filters import volume_filter, expense_ratio_filter_yf, date_filter,mst_filter
+import src.constants as c
 
 ETF_FILENAME = '../data/etf-tickers.txt'
 ETF_PREP_VOL = '../data/etf-date-vol-prep-f.txt'
 ETF_VOLUME_FILTERED_FILENAME = '../data/etf-volume-f.txt'
 ETF_ER_FILTERED_TICKERS = '../data/etf-er-f.txt'
-MINIMUM_DAILY_VOLUME = 50000
-MAXIMUM_EXPENSE_RATIO = 0.005
-VOLUME_START_DATE = '2021-05-01'
-VOLUME_END_DATE = '2022-05-01'
-
-DATE_ARRAY = ['2021-05-01','2020-05-01','2019-05-01','2018-05-01','2017-05-01']
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    # date_filter(ETF_FILENAME, VOLUME_START_DATE, 'etf', 'date-vol-prep')
-
+    # date_filter(ETF_FILENAME, c.START_DATES[-1], 'etf', 'date-vol-prep')
     # while True:
-        # expense_ratio_filter_yf(ETF_PREP_VOL, MAXIMUM_EXPENSE_RATIO)
+        # expense_ratio_filter_yf(ETF_PREP_VOL, c.MAXIMUM_EXPENSE_RATIO)
 
-    # volume_filter(ETF_ER_FILTERED_TICKERS, VOLUME_START_DATE, VOLUME_END_DATE, MINIMUM_DAILY_VOLUME, 'etf')
-
-
-    for date in DATE_ARRAY:
+    for date in c.START_DATES:
         target_name=date.split('-', maxsplit=1)[0]
         filename = [f'../data/etf-{target_name}-f.txt']
-        # date_filter(ETF_VOLUME_FILTERED_FILENAME, start_date=date, ticker_type='etf', target_name=target_name)
-        mst_filter(filename, start_date=date, end_date=VOLUME_END_DATE, target_name=target_name, ticker_type='etf', min_sr=False)
-        mst_filter(filename, start_date=date, end_date=VOLUME_END_DATE, target_name=target_name, ticker_type='etf', min_sr=True, sr_value=0)
-        mst_filter(filename, start_date=date, end_date=VOLUME_END_DATE, target_name=target_name, ticker_type='etf', min_sr=True, sr_value=1)
-        # volume_filter(f'../data/etf-{target_name}-f.txt', date, VOLUME_END_DATE, MINIMUM_DAILY_VOLUME, 'etf')
+        date_filter(ETF_ER_FILTERED_TICKERS, start_date=date, ticker_type='etf', target_name=target_name)
+        volume_filter(filename[0], date, c.END_DATE, c.MINIMUM_DAILY_VOLUME, 'etf')
+        mst_filter(filename, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='etf', min_sr=False)
+        mst_filter(filename, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='etf', min_sr=True, sr_value=0)
+        mst_filter(filename, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='etf', min_sr=True, sr_value=1)
+
         print(f"Finished date filter for {date}")
-        # time.sleep(60)
 
     print(f"Filters took {time.time() - start_time}s to run")
