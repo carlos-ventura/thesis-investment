87c15834aacee492cb796f3c533c52cc9d50c34d
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-14 23:23:38 +0200

    Joint filter - data and file

---
+++ src/Filters/joint_filter.py
@@ -0,0 +1,17 @@
+from tabnanny import filename_only
+import time
+from src.Filters.utils_filters import volume_filter, expense_ratio_filter_yf, date_filter,mst_filter
+
+import src.constants as c
+
+if __name__ == '__main__':
+    start_time = time.time()
+    # rates_filter(SOURCE_CRYPTO)
+    for date in c.START_DATES:
+        target_name=date.split('-', maxsplit=1)[0]
+        filenames = [f'../data/crypto-{target_name}-f.txt', f'../data/etf-{target_name}-f.txt']
+        mst_filter(filenames, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='etf-crypto', min_sr=False)
+        mst_filter(filenames, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='etf-crypto', min_sr=True, sr_value=0)
+        mst_filter(filenames, start_date=date, end_date=c.END_DATE, target_name=target_name, ticker_type='etf-crypto', min_sr=True, sr_value=1)
+
+    print(f"Filter took {time.time() - start_time}s to run")
