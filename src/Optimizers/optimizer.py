c2b16444eae68c7dcfca4f252349ad3f29923193
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-18 12:34:40 +0200

    Create main class for the optimizer

--- src/Optimizers/optimizer.py
+++ src/Optimizers/optimizer.py
@@ -1,7 +1,17 @@
+import itertools
+from src.Optimizers.optimizer_utils import generate_efficient_frontiers_graph, load_mst_data, optimize_variance, optimize_semivariance
+
+import src.constants as c
+from pypfopt import expected_returns, objective_functions, risk_models
 from pypfopt.efficient_frontier import EfficientFrontier
-from pypfopt import risk_models
-from pypfopt import expected_returns
-from pypfopt import objective_functions
 
 if __name__ == '__main__':
-    print('main')
+
+    for date in c.START_DATES:
+        for mst_type, mst_mode in itertools.product(c.MST_TYPES, c.MST_MODES):
+            mst_mode_print = mst_mode or 'No SR filter'
+            print(f'\n ----- {date} ----- {mst_type} ----- {mst_mode_print} -----\n')
+            returns = load_mst_data(date, mst_type, mst_mode)
+            # generate_efficient_frontiers_graph(returns)
+            optimize_variance(returns=returns, max_return = 0.1, min_risk = 0.1, l2_reg=False)
+            # optimize_semivariance()
