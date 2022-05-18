38d8fc10d93602a2a59aa4b4c2f8896e56c77723
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-18 12:33:56 +0200

    Add constant for optimizer measures working

--- src/constants.py
+++ src/constants.py
@@ -4,7 +4,7 @@ Constants to generate different results with different models
 
 WORLD_ETF_TICKERS = ["VWCE.DE", "MSCI", "ACWI"]
 
-ETF_WEIGHT = 0.8
+ETF_WEIGHT = 0.95
 CRYPTO_WEIGHT = 1 - ETF_WEIGHT
 
 START_DATES = ['2017-11-06', '2018-05-01', '2019-05-01', '2020-05-01', '2021-05-01']
@@ -12,6 +12,8 @@ END_DATE = '2022-05-01'
 
 MST_MODES = ['', 'sr0', 'sr1']
 MST_TYPES = ['separate', 'joint']
+
+OPTIMIZER_MEASURES = ['min volatility', 'efficient return', 'efficient risk']
 
 MINIMUM_DAILY_VOLUME = 50000
 MAXIMUM_EXPENSE_RATIO = 0.005
