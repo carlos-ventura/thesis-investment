5395fd78992d972cad78e2eefba98e09d77aa9a0
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-23 18:21:12 +0200

    Update constants to correct volatility and no DCA

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ Constants to generate different results with different models
 """
 
 WORLD_ETF_TICKERS = [ "IWDA.AS", "ACWI", 'VWCE.DE']
-BENCHMARK_RISK = 0.15
+BENCHMARK_RISK = 0.2
 
 ETF_WEIGHT = 0.95
 CRYPTO_WEIGHT = 1 - ETF_WEIGHT
@@ -14,10 +14,9 @@ END_DATE = '2022-05-01'
 MST_MODES = ['', 'sr0', 'sr1']
 MST_TYPES = ['separate', 'joint']
 
-OPTIMIZER_MEASURES = ['min volatility'] # efficient risk, min volatility
+OPTIMIZER_MEASURES = ['efficient risk'] # efficient risk, min volatility
 
 MINIMUM_DAILY_VOLUME = 2000000 # Dollars
 MAXIMUM_EXPENSE_RATIO = 0.005
 MAXIMUM_ANNUAL_STD = 2
 
-DCA_MONTHLY = 0  # default
