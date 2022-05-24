f1a4973348e99d3c2ebe49d7b15bd7924dcc3de4
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-24 16:55:35 +0200

    Updates to optimnizers

--- src/Optimizers/optimizer.py
+++ src/Optimizers/optimizer.py
@@ -8,9 +8,15 @@ from src.Optimizers.optimizer_utils import benchmark_stats, load_benchmark, prin
 import src.constants as c
 
 DICT_BENCHMARK_STATS = collections.defaultdict(dict)
-DICT_ETF = collections.defaultdict(dict)
-DICT_ETF_CRYPTO_MST = collections.defaultdict(dict)
-DICT_ETF_CRYPTO_TOP = collections.defaultdict(dict)
+DICT_ETF_MST = collections.defaultdict(dict)
+DICT_ETF_MST_CRYPTO_MST = collections.defaultdict(dict)
+DICT_ETF_MST_CRYPTO_MST_PASSIVE = collections.defaultdict(dict)
+
+
+DICT_CRYPTO_APY = collections.defaultdict(dict)
+
+with open('../data/rates.json') as json_file:
+    DICT_CRYPTO_APY = json.load(json_file)
 
 def etf_benchmark():
     for date in c.START_DATES:
@@ -19,24 +25,55 @@ def etf_benchmark():
         DICT_BENCHMARK_STATS[date]['benchmark'] = stats_benchmark_dict
     write_json(DICT_BENCHMARK_STATS, "benchmark_stats.json")
 
-def etf_optimizer():
+def etf_mst_optimizer():
+    sector=False
+    rebalance=True
     for date in c.START_DATES:
+        print(date)
+        for mst_mode in c.MST_MODES:
+            mst_mode_print = mst_mode or 'No sr'
+            print(mst_mode_print)
+            DICT_ETF_MST[date][mst_mode_print]=[]
+            l = [False, True]
+            bools_list = [list(i) for i in itertools.product(l, repeat=2)]
+            for i, bools in enumerate(bools_list):
+                returns = load_mst_data(date, mst_type=None, mst_mode=mst_mode, etf=True)
+                train, test = train_test_split(returns, train_size=0.3, shuffle=False)
+                # print_efficient_frontiers_graph(returns=train, title=f"{date} {mst_mode_print}",l2_reg=bools[0], min_weights=bools[1])
+                # print_correlation_heatmap(train, title)
+                in_sample, out_sample, non_zero_weights, weights = optimize_variance(
+                    returns=returns,
+                    train=train,
+                    test=test,
+                    l2_reg=bools[0],
+                    min_weights=bools[1],
+                    sector=sector,
+                    rebalance=rebalance,
+                    rebalance_weeks=52
+                    )
+
+                DICT_ETF_MST[date][mst_mode_print].append({i: {"nr_weights": len(non_zero_weights), "nr_0_weights": len(weights) - len(non_zero_weights),
+                 "l2_reg": bools[0], "min_weights": bools[1], "train": in_sample, "test": out_sample }})
+                 
+    if rebalance: write_json(DICT_ETF_MST, "etf_mst_stats_rebalance.json")
+    else: write_json(DICT_ETF_MST, "etf_mst_stats.json")
+
+def etf_mst_crypto_mst_optimizer():
+    for date in c.START_DATES:
+        print(date)
         for mst_type, mst_mode in itertools.product(c.MST_TYPES, c.MST_MODES):
             mst_mode_print = mst_mode or 'No SR filter'
-            title = f"{date} {mst_type} {mst_mode_print}"
-            print(f'\n{title}')
-            returns = load_mst_data(date, mst_type, mst_mode)
+            modes = f"{mst_type} {mst_mode_print}"
+            print(f'{modes}')
+            returns = load_mst_data(date, mst_type, mst_mode, etf=True)
             train, test = train_test_split(returns, train_size=0.3, shuffle=False)
             # print_efficient_frontiers_graph(train, title)
             # print_correlation_heatmap(train, title)
             in_sample, out_sample = optimize_variance(returns=returns, train=train, test=test, max_return = 0.1, min_risk = c.BENCHMARK_RISK, l2_reg=True)
             # optimize_semivariance()
 
-def etf_crypto_optimizer():
-    pass
-
-def etf_crypto_passive_optimizer():
+def etf_mst_crypto_mst_apy_optimizer():
     pass
 
 if __name__ == '__main__':
-    etf_benchmark()
+    etf_mst_optimizer()
--- src/Optimizers/optimizer_utils.py
+++ src/Optimizers/optimizer_utils.py
@@ -1,14 +1,11 @@
 import json
+import math
 from statistics import mean
-from matplotlib import pyplot as plt
-import numpy as np
 import pandas as pd
 from pypfopt import EfficientSemivariance, expected_returns, objective_functions, risk_models, plotting, HRPOpt
 from pypfopt.efficient_frontier import EfficientFrontier
 import plotly.graph_objects as go
-import plotly.figure_factory as ff
 import plotly_express as px
-from pandas.testing import assert_frame_equal
 from sklearn.model_selection import train_test_split
 import collections
 import empyrical as ep
@@ -16,29 +13,70 @@ import empyrical as ep
 import src.constants as c
 from src.utils import annualized_return, annualized_std
 
-def load_mst_data(date:str, mst_type:str, mst_mode:str):
+def load_mst_data(date:str, mst_type:str, mst_mode:str, etf=True, crypto=False, passive=False, passive_mode = "mean"):
     year = date.split('-', maxsplit=1)[0]
     path = '../data/mst/pickle/'
     mode_path = f'-{mst_mode}-' if mst_mode else '-'
     if mst_type == 'joint':
         return pd.read_pickle(f'{path}etf-crypto{mode_path}{year}.pkl')
 
-    returns_crypto = pd.read_pickle(f'{path}crypto{mode_path}{year}.pkl')
     returns_etf = pd.read_pickle(f'{path}etf{mode_path}{year}.pkl')
 
-    return pd.concat([returns_etf, returns_crypto], axis=1, join='inner')
+    if etf:
+        return returns_etf
+    if crypto:
+        returns_crypto = pd.read_pickle(f'{path}crypto{mode_path}{year}.pkl')
+        if passive:
+            return pd.concat([returns_etf ,get_crypto_returns_passive(returns_crypto, passive_mode)], axis=1, join="inner") 
+        return pd.concat([returns_etf, returns_crypto], axis=1, join='inner')
 
-def optimize_variance(returns, train, test, max_return:float, min_risk:float, l2_reg = False):
+
+def optimize_variance(returns, train, test, l2_reg=False, min_weights=False, sector=False, rebalance=False, rebalance_weeks=52):
+    in_sample_dict = collections.defaultdict(dict)
+    out_sample_dict = collections.defaultdict(dict)
     for opt_mes in c.OPTIMIZER_MEASURES:
-        ef = generate_ef(train, sector=True, l2_reg=l2_reg)
-        weights = optimizer_measures_weights(ef, opt_mes, max_return, min_risk)
-        cleaned_weights = ef.clean_weights()
+        if not rebalance:
+            ef_train = generate_ef(train, sector=sector, l2_reg=l2_reg, l2_value=0.1, min_weights=min_weights)
+            weights = optimizer_measures_weights(ef_train, opt_mes)
+            cleaned_weights = ef_train.clean_weights()
+            non_zero_weights = {x:y for x,y in cleaned_weights.items() if y!=0}
+            print(non_zero_weights)
+            print("Total weights: ", len(cleaned_weights), " :::  Weights not used: ", len(cleaned_weights) - len(non_zero_weights))
 
-        # print(cleaned_weights)
-        print({x:y for x,y in cleaned_weights.items() if y!=0})
+            mu_train, sigma_train, _= ef_train.portfolio_performance(verbose=True)
 
-        print_performance_title(opt_mes)
-        ef.portfolio_performance(verbose=True)
+            in_sample_dict[opt_mes] = {'return': round(mu_train * 100, 2), 'std': round(sigma_train * 100, 2)}
+
+            port_returns = pd.Series(weights) * test
+            port_returns = port_returns.sum(axis=1).to_frame()
+
+            ef_test = generate_ef(test)
+            ef_test.set_weights(weights)
+            mu_test, sigma_test , _ = ef_test.portfolio_performance(verbose=True)
+            real_mu_test = float(ep.annual_return(port_returns, period="weekly"))
+
+
+        if rebalance:
+            mu_test = 0
+            rebalanced_port = pd.DataFrame()
+            for count in chunks(test.shape[0], rebalance_weeks):
+                ef_train = generate_ef(train, sector=sector, l2_reg=l2_reg, l2_value=0.1, min_weights=min_weights)
+                weights = optimizer_measures_weights(ef_train, opt_mes)
+                cleaned_weights = ef_train.clean_weights()
+                non_zero_weights = {x:y for x,y in cleaned_weights.items() if y!=0}
+                rebalanced_port = pd.concat([rebalanced_port, pd.Series(cleaned_weights) * test.head(count)])
+                rebalanced_port = rebalanced_port
+                train = pd.concat([train, test.head(count)])
+                test = test.drop(test.index[range(count)])
+            rebalanced_port = rebalanced_port.loc[:, (rebalanced_port != 0).any(axis=0)]
+            non_zero_weights = rebalanced_port.columns.values
+            rebalanced_returns = rebalanced_port.sum(axis=1)
+            sigma_test = float(ep.annual_volatility(rebalanced_returns, period="weekly"))
+            real_mu_test = float(ep.annual_return(rebalanced_returns, period="weekly"))
+
+        out_sample_dict[opt_mes] = {'return': round(mu_test * 100, 2), 'real return': round(real_mu_test * 100, 3), 'std': round(sigma_test * 100, 2)}
+
+    return in_sample_dict, out_sample_dict, non_zero_weights, weights
 
 def optimize_semivariance(returns):
     pass
@@ -57,7 +95,7 @@ def optimizer_measures_weights(ef: EfficientFrontier, opt_mes:str, max_return = 
     if opt_mes == 'efficient risk':
         return ef.efficient_risk(min_risk)
 
-def print_efficient_frontiers_graph(returns:pd.DataFrame, title:str):
+def print_efficient_frontiers_graph(returns:pd.DataFrame, title:str, l2_reg:bool, min_weights:bool):
     asset_names = returns.columns.values
     crypto = []
     etf = []
@@ -70,20 +108,20 @@ def print_efficient_frontiers_graph(returns:pd.DataFrame, title:str):
     returns_crypto = returns[crypto]
     returns_etf = returns[etf]
 
-    ef_crypto = generate_ef(returns_crypto, sector=False, l2_reg=True)
-    ef_etf = generate_ef(returns_etf, sector=False, l2_reg=True)
-    ef_combined = generate_ef(returns, sector=True, l2_reg=True)
+    # ef_crypto = generate_ef(returns_crypto, sector=False, l2_reg=l2_reg, min_weights=min_weights)
+    ef_etf = generate_ef(returns_etf, sector=True, l2_reg=l2_reg, min_weights=min_weights)
+    # ef_combined = generate_ef(returns, sector=False, l2_reg=l2_reg, min_weights=min_weights)
 
-    _, mus_crypto , sigmas_crypto, assets_crypto = plotting.plot_efficient_frontier(ef_crypto, ef_param='return')
+    # _, mus_crypto , sigmas_crypto, assets_crypto = plotting.plot_efficient_frontier(ef_crypto, ef_param='return')
     _, mus_etf , sigmas_etf, assets_etf = plotting.plot_efficient_frontier(ef_etf, ef_param='return')
-    _, mus_combined , sigmas_combined, _ = plotting.plot_efficient_frontier(ef_combined, ef_param='return', show_assets=False)
+    # _, mus_combined , sigmas_combined, _ = plotting.plot_efficient_frontier(ef_combined, ef_param='return', show_assets=False)
 
     f1 = go.Figure(
     data = [
-        go.Scatter(x=sigmas_crypto,y=mus_crypto, name='Efficient Frontier Crypto'),
+      #  go.Scatter(x=sigmas_crypto,y=mus_crypto, name='Efficient Frontier Crypto'),
         go.Scatter(x=sigmas_etf, y=mus_etf, name="Efficient Frontier ETF"),
-        go.Scatter(x=sigmas_combined, y=mus_combined, name='Efficient Frontier Crypto + ETF'),
-        go.Scatter(x=assets_crypto['sigmas'], y = assets_crypto['mus'], name='Cryptos', mode='markers'),
+       # go.Scatter(x=sigmas_combined, y=mus_combined, name='Efficient Frontier Crypto + ETF'),
+        #go.Scatter(x=assets_crypto['sigmas'], y = assets_crypto['mus'], name='Cryptos', mode='markers'),
         go.Scatter(x=assets_etf['sigmas'], y = assets_etf['mus'], name='ETFs', mode='markers'),
     ],
     layout = go.Layout(
@@ -97,11 +135,15 @@ def print_correlation_heatmap(returns:pd.DataFrame, title:str):
     fig = px.imshow(returns.corr(), title=f"Heatmap Correlation : {title}")
     fig.show()
 
-def generate_ef(returns:pd.DataFrame, sector:bool = True, l2_reg = False, l2_value = 0.1, verbose=False):
-    mu = expected_returns.mean_historical_return(returns, returns_data=True ,compounding=True, frequency=52)
-    S = risk_models.sample_cov(returns, returns_data=True, frequency=52)
-
-    ef = EfficientFrontier(mu, S, verbose=verbose, solver="SCS")
+def generate_ef(returns:pd.DataFrame, sector:bool = True, l2_reg = False, min_weights = False, l2_value=0.1, verbose=False, semivariance=False):
+    mu = expected_returns.mean_historical_return(returns, returns_data=True, compounding=True, frequency=52)
+    
+    if semivariance:
+        S = risk_models.semicovariance(returns, returns_data=True, frequency=52)
+        ef = EfficientSemivariance(mu, returns, verbose=verbose, solver="SCS", frequency=52)
+    else:
+        S = risk_models.sample_cov(returns, returns_data=True, frequency=52)
+        ef = EfficientFrontier(mu, S, verbose=verbose, solver="SCS", solver_options={"max_iters": 999999})
 
     if sector:
         sector_mapper = {asset: 'crypto' if '-USD' in asset else 'etf' for asset in returns.columns.values}
@@ -112,7 +154,15 @@ def generate_ef(returns:pd.DataFrame, sector:bool = True, l2_reg = False, l2_val
     if l2_reg:
         ef.add_objective(objective_functions.L2_reg, gamma=l2_value) # Reduce 0% weights
 
+    if min_weights:
+        ef.add_constraint(lambda x : x >= 0.001)
+
     return ef
+
+def get_crypto_returns_passive(returns:pd.DataFrame, passive_mode):
+    cryptos = returns.columns.values
+    for crypto in cryptos:
+        apy_list = get_crypto_apys(crypto)
 
 def load_benchmark(date):
     year = date.split('-', maxsplit=1)[0]
@@ -122,7 +172,7 @@ def load_benchmark(date):
 def benchmark_stats(returns):
     train, test = train_test_split(returns, train_size=0.3, shuffle=False)
     individual = {}
-    return_train, return_test, std_train, std_test, return_all, std_all = [], [], [], [], [], []
+    return_train, return_test, std_train, std_test = [], [], [], []
     for bench in c.WORLD_ETF_TICKERS:
         ret_train = annualized_return(train[bench])
         ret_test = annualized_return(test[bench])
@@ -143,4 +193,10 @@ def write_json(dict:dict, name:str):
     with open(f"../data/{name}", "w") as outfile:
         json.dump(dict, outfile, indent=4)
 
-
+def chunks(n:int, size:int):
+    division = math.floor(n / size)
+    rest = n % size
+    out = [size for _ in range(division - 1)]
+    if rest != 0:
+        out.append(rest)
+    return out
