f4c33ab5c4f2fa38992680e5f03485ec72f167d4
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-26 00:13:46 +0200

    Update constant include min volatility to help with calculations

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,7 @@ WORLD_ETF_TICKERS = [ "IWDA.AS", "ACWI", 'VWCE.DE']
 BENCHMARK_RISK = 0.2
 
 ETF_WEIGHT = 0.95
-CRYPTO_WEIGHT = 1 - ETF_WEIGHT
+CRYPTO_WEIGHT = 0.05
 
 START_DATES = ['2017-11-06', '2019-05-01']
 END_DATE = '2022-05-01'
@@ -14,7 +14,7 @@ END_DATE = '2022-05-01'
 MST_MODES = ['', 'sr0', 'sr1']
 MST_TYPES = ['separate', 'joint']
 
-OPTIMIZER_MEASURES = ['efficient risk'] # efficient risk, min volatility
+OPTIMIZER_MEASURES = ['min volatility','efficient risk'] # efficient risk, min volatility
 
 MINIMUM_DAILY_VOLUME = 2000000 # Dollars
 MAXIMUM_EXPENSE_RATIO = 0.005
