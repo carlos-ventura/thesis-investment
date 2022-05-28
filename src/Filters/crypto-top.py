4b421a1b642e8e5718e7a074282a1ed7b11a3587
carlos-ventura <carlospinho_pt@hotmail.com>
2022-05-28 02:39:05 +0200

    download date stablecoins

--- src/Filters/crypto-top.py
+++ src/Filters/crypto-top.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pandas import Series
 import yfinance
 from src.Filters.utils_filters import date_filter
 import src.constants as c
@@ -8,14 +9,16 @@ def filter_download_top_crypto():
         target_name=date.split('-', maxsplit=1)[0]
 
         _ = date_filter(
-            '../data/crypto-top30-mcap.txt',
-            start_date=date, ticker_type='crypto-top30',
+            '../data/crypto-top10-stablecoins-mcap.txt',
+            start_date=date, ticker_type='crypto-top10-stable',
             target_name=target_name)
 
-        with open(f"../data/crypto-top30-{target_name}-f.txt") as ticker_file:
+        with open(f"../data/crypto-top10-stable-{target_name}-f.txt") as ticker_file:
             tickers = ticker_file.read().split('\n')
 
         tickers_data = yfinance.download(tickers, start=date, end=c.END_DATE, interval="1wk")["Adj Close"]
+        if type(tickers_data) is Series:
+            tickers_data = tickers_data.to_frame()
         tickers_data.dropna(how='all', inplace=True)
         tickers_returns = tickers_data.pct_change()[1:] # Remove first row of NaN value
 
@@ -24,7 +27,9 @@ def filter_download_top_crypto():
 
         new_tickers=tickers_returns.columns
 
-        tickers_returns[new_tickers].to_pickle(f"../data/mst/pickle/crypto-top30-{target_name}.pkl")
+        tickers_returns[new_tickers].to_pickle(f"../data/mst/pickle/crypto-top10-stable-{target_name}.pkl")
+
+        # Test read from pickle
 
 
 if __name__ == '__main__':
